70f4a3f2bf811e6c8005451550cc6757e737158a
Kushagra Pant <208350034+kushagra0018@users.noreply.github.com>
2026-09-17 23:31:02 +0530

    first commit

---
+++ Day39/Q77.c
@@ -0,0 +1,57 @@
+/*Q77: Check if the elements on the diagonal of a matrix are distinct.
+
+
+Sample Test Cases:
+Input 1:
+3 3
+1 2 3
+4 5 6
+7 8 1
+Output 1:
+False
+
+Input 2:
+3 3
+1 2 3
+4 5 6
+7 8 9
+Output 2:
+True
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    int a[n][m];
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    int distinct = 1;
+
+    for (int i = 0; i < n && i < m; i++) {
+        for (int j = i + 1; j < n && j < m; j++) {
+            if (a[i][i] == a[j][j]) {
+                distinct = 0;
+                break;
+            }
+        }
+
+        if (!distinct)
+            break;
+    }
+
+    if (distinct)
+        printf("True");
+    else
+        printf("False");
+
+    return 0;
+}
