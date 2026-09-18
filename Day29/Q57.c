0bcf95e02369eed52abf77c05bf519e90057a40e
Kushagra Pant <208350034+kushagra0018@users.noreply.github.com>
2026-09-18 13:00:04 +0530

    first commit

--- Day29/Q57.c
+++ Day29/Q57.c
@@ -24,7 +24,7 @@ int main() {
     scanf("%d", &n);
 
     int arr[n];
-
+    printf("Enter the elements of the array: ");
     for (i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
         sum = sum + arr[i];
--- Day29/Q58.c
+++ Day29/Q58.c
@@ -20,11 +20,11 @@ Max=10, Min=10
 
 int main() {
     int n, i;
-    
+    printf("Enter the number of elements: ");
     scanf("%d", &n);
 
     int arr[n];
-
+    printf("Enter the elements of the array: ");
     for (i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
