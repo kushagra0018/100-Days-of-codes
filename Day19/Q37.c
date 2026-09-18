da1f5c2838f2fc808e29df21d0e71f31446cd19d
Kushagra Pant <208350034+kushagra0018@users.noreply.github.com>
2026-09-18 15:12:19 +0530

    first commit

--- Day19/Q37.c
+++ Day19/Q37.c
@@ -1,36 +1,25 @@
-/*Q37: Write a program to find the LCM of two numbers.
-
-
-Sample Test Cases:
-Input 1:
-4 5
-Output 1:
-20
-
-Input 2:
-7 3
-Output 2:
-21
-
-*/
-
-#include <stdio.h>
+ #include <stdio.h>
 
 int main() {
-    int a, b, lcm;
+    int a, b, x, y, hcf, lcm;
 
+    printf("Enter two numbers: ");
     scanf("%d %d", &a, &b);
 
-    lcm = (a > b) ? a : b;
+    x = a;
+    y = b;
 
-    while (1) {
-        if (lcm % a == 0 && lcm % b == 0) {
-            break;
-        }
-        lcm++;
+    while (y != 0) {
+        int temp = y;
+        y = x % y;
+        x = temp;
     }
 
-    printf("%d", lcm);
+    hcf = x;
+
+    lcm = (a * b) / hcf;
+
+    printf("LCM = %d", lcm);
 
     return 0;
 }
