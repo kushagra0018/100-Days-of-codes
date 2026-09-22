18a88f7042a6395340a605e071124b442c75b4e1
Kushagra Pant <208350034+kushagra0018@users.noreply.github.com>
2026-09-22 09:19:05 +0530

    First commit

--- Day39/Q77.c
+++ Day39/Q77.c
@@ -28,7 +28,6 @@ int main() {
     scanf("%d %d", &n, &m);
 
     int a[n][m];
-
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             printf("Enter element at position (%d, %d): ", i + 1, j + 1);
